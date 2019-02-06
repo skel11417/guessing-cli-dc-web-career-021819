1bf94d248bb0dffc1b99ceacc6221faf3bb12361
Sean Kelly <shonkelli@gmail.com>
2019-02-06 18:56:04 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,6 @@
 def run_guessing_game
   comp_guess = 5
   puts "Guess a number between 1 and 6."
-  puts "The computer guessed #{comp_guess}"
+  puts "The computer guessed #{comp_guess}."
   puts "Goodbye!"
 end
